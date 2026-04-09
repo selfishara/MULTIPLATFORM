28c898ee9a2df659e687f1cbfd1988379337e8ed
sara <saramb10.smb@alumnes.ilerna.com>
2026-04-09 18:05:51 +0200

    Initial setup

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -8,6 +8,8 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.composeHotReload)
+    alias(libs.plugins.kotlinSerialization)
+
 }
 
 kotlin {
@@ -54,6 +56,8 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.jetbrains.navigation3.ui)
+            implementation(libs.kotlinx.serialization.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
