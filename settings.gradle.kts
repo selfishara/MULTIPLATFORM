e5f7072b768ad7bb9ac1e34afa73e114fbccd8a0
Selfishara <saramb10.smb@gmail.com>
2026-04-11 16:56:36 +0200

    bug fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,14 +17,9 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
-        google {
-            mavenContent {
-                includeGroupAndSubgroups("androidx")
-                includeGroupAndSubgroups("com.android")
-                includeGroupAndSubgroups("com.google")
-            }
-        }
+        google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
